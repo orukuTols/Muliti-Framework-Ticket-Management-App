78e2e253ac6aeb01d86b516bdb39684f01e6eb61
Oruku Toluwalase <orukutoluwalase@gmail.com>
2025-10-31 02:04:26 +0100

    Deleted and reinstalled the files needed

--- Vue/vite.config.js
+++ Vue/vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// This file uses the ES Module syntax (import/export) because your package.json
-// is set to "type": "module", resolving the CommonJS error.
+
 export default defineConfig({
   plugins: [
     vue(),
   ],
-  // This CSS block explicitly tells Vite to use your manual PostCSS setup,
-  // which is crucial for Tailwind to compile.
+
   css: {
     postcss: {
       config: './postcss.config.cjs',
